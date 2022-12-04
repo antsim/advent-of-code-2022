8ca219ccadc82949d19e15f295c051178bdc5622
Antti Simonen <antti.simonen@gofore.com>
2022-12-04 11:25:49 +0200

    chore: day4 solution

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Solutions and tests for [Advent of Code](https://adventofcode.com/) puzzles.
 
 Built with the [Advent of Code Elixir Starter](https://github.com/mhanberg/advent-of-code-elixir-starter).
+
+This is the first Elixir code that I ever write. Some of the solutions come up with the help of Elixir AOC forums and the nice solutions people share there. 
+Credit goes to the people there.
--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,46 @@
 defmodule AdventOfCode.Day04 do
-  def part1(args) do
+  import Bitwise
+
+  def part1(input) do
+    input
+    |> parse_input()
+    |> Enum.filter(fn line ->
+      line
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(fn range ->
+        [s,e] = range |> String.split("-") |> Enum.map(& String.to_integer(&1))
+        Integer.pow(2, e - s + 1) - 1 <<< (s - 1)
+      end)
+      |> Enum.reduce(fn int1, int2 ->
+        intersection = int1 ||| int2
+        intersection == int1 or intersection == int2
+      end)
+    end)
+    |> Enum.count()
   end
 
-  def part2(args) do
+  def part2(input) do
+    input
+    |> parse_input()
+    |> Enum.filter(fn line ->
+      line
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(fn range ->
+        [s,e] = range |> String.split("-") |> Enum.map(& String.to_integer(&1))
+        Integer.pow(2, e - s + 1) - 1 <<< (s - 1)
+      end)
+      |> Enum.reduce(fn int1, int2 ->
+        (int1 &&& int2) != 0
+      end)
+    end)
+    |> Enum.count()
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
   end
 end
